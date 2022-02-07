457100320aaf8cfcd5e0fb344b10079551a7c519
Nicklas Chapman <nicklas.chapman@outlook.com>
2022-02-07 21:56:49 +0000

    Updated so that the struct signatur matches the interface for ViewPort

--- pkg/viewport/viewport.go
+++ pkg/viewport/viewport.go
@@ -1,21 +1,14 @@
 package viewport
 
 import (
-	"errors"
-
 	"github.com/realency/arke/pkg/display"
 )
 
 type ViewPort interface {
-	Attach(canvas *display.Canvas, row, col int) error
-	Detach() error
-	Shift(rowDelta, colDelta int) error
+	Attach(canvas *display.Canvas, row, col int)
+	Detach()
+	Locate(row, col int)
 	Offset() (row, col int)
 	Size() (height, width int)
 	Canvas() *display.Canvas
 }
-
-var (
-	EAlreadyAttached = errors.New("ViewPort already attached to a canvas")
-	ENotAttached     = errors.New("ViewPort not attached to a canvas")
-)
