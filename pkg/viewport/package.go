a67b3c4ebde5ca1fa83a2a2b18a7091677893744
Nicklas Chapman <nicklas.chapman@outlook.com>
2022-02-19 02:54:40 +0000

    More godoc commenting

---
+++ pkg/viewport/package.go
@@ -0,0 +1,3 @@
+// Package viewport provides a standard interface for adaptors that drive dot-matrix displays from
+// a logical representation in the form of a display.Canvas.
+package viewport
--- pkg/viewport/viewport.go
+++ pkg/viewport/viewport.go
@@ -4,6 +4,12 @@ import (
 	"github.com/realency/arke/pkg/display"
 )
 
+// ViewPort is the interface for types that connect a Canvas to a physical dot-matrix display.
+//
+// A ViewPort can be conceptualised as a frame surrounding a subregion of pixels in a Canvas.
+// Pixels within the frame are represented in the display, and changes made to the canvas effect
+// the same change in the display.  Allowing a viewport to attach to a subregion of the canvas
+// allows for effects such as scrolling, in which the viewport moves over the underlying canvas.
 type ViewPort interface {
 	Attach(canvas *display.Canvas, row, col int)
 	Detach()
