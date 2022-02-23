d8a8679593f0135f8150f10ffcfff7f2b01cbe99
Nicklas Chapman <nicklas.chapman@outlook.com>
2022-02-23 19:02:28 +0000

    Now fully documented

--- pkg/max7219/builders.go
+++ pkg/max7219/builders.go
@@ -8,6 +8,7 @@ import (
 	"periph.io/x/host/v3"
 )
 
+// BusBuilder is a builder type for creating a Bus in a fluent programming style.
 type BusBuilder struct {
 	dev  string
 	port spi.Port
@@ -15,6 +16,7 @@ type BusBuilder struct {
 	bus  Bus
 }
 
+// ViewPortBuilder is a builder type for creating a ViewPort in a fluent programming style.
 type ViewPortBuilder struct {
 	bus              *BusBuilder
 	blockOrientation int
@@ -22,34 +24,40 @@ type ViewPortBuilder struct {
 	chainLength      int
 }
 
+// FromScratch creates a new BusBuilder appropriate for building a bus from scratch using the default SPI device.
 func FromScratch() *BusBuilder {
 	return FromDeviceName("")
 }
 
+// FromDeviceName creates a new BusBuilder appropriate for building a bus using a specific SPI device, identified by device name.
 func FromDeviceName(dev string) *BusBuilder {
 	return &BusBuilder{
 		dev: dev,
 	}
 }
 
+// FromSpiPort creates a new BusBuilder appropriate for building a bus using a pre-created SPI Port.
 func FromSpiPort(port spi.Port) *BusBuilder {
 	return &BusBuilder{
 		port: port,
 	}
 }
 
+// FromConnection creates a new BusBuilder appropriate for building a bus using a pre-created Connection.
 func FromConnection(cx conn.Conn) *BusBuilder {
 	return &BusBuilder{
 		cx: cx,
 	}
 }
 
+// FromBus creates a new BusBuilder that returns a pre-created bus.
 func FromBus(bus Bus) *BusBuilder {
 	return &BusBuilder{
 		bus: bus,
 	}
 }
 
+// Build builds a Bus using the configuration supplied to the builder, or returns an error if the configuration is incomplete.
 func (b *BusBuilder) Build() (Bus, error) {
 	var err error
 
@@ -75,6 +83,7 @@ func (b *BusBuilder) Build() (Bus, error) {
 	return newBus(b.cx), nil
 }
 
+// WithChainLength specifies the chain length for the bus and returns a ViewPortBuilder.
 func (b *BusBuilder) WithChainLength(length int) *ViewPortBuilder {
 	return &ViewPortBuilder{
 		bus:         b,
@@ -82,12 +91,14 @@ func (b *BusBuilder) WithChainLength(length int) *ViewPortBuilder {
 	}
 }
 
+// WithOrientation specifies the orientation of the blocks in the chains, and of the chain itself and returns the ViewPortBuilder.
 func (v *ViewPortBuilder) WithOrientation(blockOrientation, chainOrientation int) *ViewPortBuilder {
 	v.blockOrientation = blockOrientation
 	v.chainOrientation = chainOrientation
 	return v
 }
 
+// Build builds the viewport, ready to be attached to a canvas.
 func (v *ViewPortBuilder) Build() (*ViewPort, error) {
 	b, err := v.bus.Build()
 	if err != nil {
